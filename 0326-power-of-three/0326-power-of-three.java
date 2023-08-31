7b883f82fe2c580419ea9b746fc8878e9060d142
Keshav Kumar Mishra <95664908+keshavMishra29@users.noreply.github.com>
2023-08-31 22:36:23 +0530

    Time: 8 ms (86.53%), Space: 42.6 MB (79.26%) - LeetHub

---
+++ 0326-power-of-three/0326-power-of-three.java
@@ -0,0 +1,7 @@
+class Solution {
+    public boolean isPowerOfThree(int n) {
+       if(n==0 ) return false;
+        else if((n==1) || (n%3==0 && isPowerOfThree(n/3)))return true;
+        else return false; 
+    }
+}
