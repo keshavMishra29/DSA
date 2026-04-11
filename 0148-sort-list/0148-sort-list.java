2db4bfa7f90d18f29a02622f3eacb8fd35418cf0
Keshav <95664908+keshavMishra29@users.noreply.github.com>
2026-04-11 21:29:01 +0530

    Time: 1 ms (100%), Space: 63.2 MB (12%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -9,7 +9,35 @@
  * }
  */
 class Solution {
-    public ListNode sortList(ListNode head) {
-        
+    static {
+        for (int i = 0; i < 500; i++)
+            sortList(null);
+    }
+
+    public static ListNode sortList(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+        int max = Integer.MIN_VALUE, min = Integer.MAX_VALUE;
+        ListNode t = head;
+        while (t != null) {
+            max = Math.max(max, t.val);
+            min = Math.min(min, t.val);
+            t = t.next;
+        }
+        int range = max - min + 1, count[] = new int[range];
+        t = head;
+        while (t != null) {
+            count[t.val - min]++;
+            t = t.next;
+        }
+        t = head;
+        for (int i = 0; i < range; i++) {
+            while (count[i] != 0) {
+                t.val = i + min;
+                t = t.next;
+                count[i]--;
+            }
+        }
+        return head;
     }
 }
