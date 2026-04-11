8959e373a25445c39d04316a9c93b475f23ca74f
Keshav <95664908+keshavMishra29@users.noreply.github.com>
2026-04-11 23:13:45 +0530

    Time: 0 ms (100%), Space: 43.3 MB (68.05%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,15 +1,19 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-    for (int i = digits.length - 1; i >= 0; i--) {
-	if (digits[i] < 9) {
-		digits[i]++;
-		return digits;
-	}
-	digits[i] = 0;
-}
-
-digits = new int[digits.length + 1];
-digits[0] = 1;
-return digits;
+        // Traverse from last digit
+        for (int i = digits.length - 1; i >= 0; i--) {
+            // If digit is less than 9, just increment and return
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            // If digit is 9, set it to 0 and continue
+            digits[i] = 0;
+        }
+        
+        // If all digits were 9, create new array
+        int[] result = new int[digits.length + 1];
+        result[0] = 1; // rest are already 0
+        return result;
     }
 }
