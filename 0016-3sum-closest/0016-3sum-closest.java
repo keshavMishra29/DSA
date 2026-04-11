5a4ed65976bbf60d98945e9951f28d1e374d69c4
Keshav <95664908+keshavMishra29@users.noreply.github.com>
2026-04-11 23:22:16 +0530

    Time: 6 ms (100%), Space: 45.8 MB (19.79%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -1,27 +1,58 @@
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
+        int n = nums.length;
         Arrays.sort(nums);
-        int closestSum = nums[0] + nums[1] + nums[2]; // Initialize closest sum with the sum of the first three elements
 
-        for (int i = 0; i < nums.length - 2; i++) {
-            int j = i + 1;
-            int k = nums.length - 1;
-
-            while (j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
-
-                if (Math.abs(target - sum) < Math.abs(target - closestSum)) {
-                    closestSum = sum; // Update closest sum if the current sum is closer to the target
-                }
-
-                if (sum < target) {
-                    j++; // Increment j to increase the sum
-                } else {
-                    k--; // Decrement k to decrease the sum
-                }
-            }
+        int smallest = nums[0] + nums[1] + nums[2];
+        if (smallest >= target || n == 3) {
+            return smallest;
         }
 
-        return closestSum;
+        int largest = nums[n - 3] + nums[n - 2] + nums[n - 1];
+        if (largest <= target) {
+            return largest;
+        }
+
+        int closest = largest - target < target - smallest ? largest : smallest;
+        int l = 0, r = n - 1;
+
+        while (r > l + 1 && nums[l] + nums[l + 1] + nums[r] > target) {
+            int sum = nums[l] + nums[l + 1] + nums[r];
+            closest = sum - target < Math.abs(target - closest) ? sum : closest;
+            r--;
+        }
+
+        while (l < r - 1) {
+            int sum = closest;
+            while (l < r - 1 && nums[l] + nums[l + 1] + nums[r] < target) {
+                sum = nums[l] + nums[l + 1] + nums[r];
+                l++;
+            }
+
+            closest = Math.abs(target - sum) < Math.abs(target - closest) ? sum : closest;
+            int mid = l + 1;
+
+            while (l < r - 1 && (0 <= l || mid <= r - 1)) {
+                sum = nums[l] + nums[mid] + nums[r];
+                closest = Math.abs(target - sum) < Math.abs(target - closest) ? sum : closest;
+
+                if (sum > target) {
+                    if (l == 0)
+                        break;
+                    l--;
+                } else if (sum < target) {
+                    if (mid == r - 1)
+                        break;
+                    mid++;
+                } else {
+                    return closest;
+                }
+            }
+
+            r--;
+            l = 0;
+        }
+
+        return closest;
     }
 }
