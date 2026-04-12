00376cbe4ca878faf7260fc972f781fb55c5e40c
Keshav <95664908+keshavMishra29@users.noreply.github.com>
2026-04-12 11:14:51 +0530

    Time: 2 ms (99.57%), Space: 42.2 MB (47.32%) - LeetHub

---
+++ 1015-smallest-integer-divisible-by-k/1015-smallest-integer-divisible-by-k.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int smallestRepunitDivByK(int k) {
+        if (k % 2 == 0 || k % 5 == 0) return -1; // divisible by 2 or 5 impossible
+
+        int rem = 0;
+        for (int len = 1; len <= k; len++) {
+            rem = (rem * 10 + 1) % k;
+            if (rem == 0) return len;
+        }
+        return -1;
+    }
+}
